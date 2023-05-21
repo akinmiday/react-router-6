1f231c2c3d0165603add1a7a48690e04e128e360
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-05-21 04:53:07 +0100

    Js server added

--- react-router-6/src/App.jsx
+++ react-router-6/src/App.jsx
@@ -1,8 +1,11 @@
+import React from 'react'
 import './App.css'
 import { Routes, Route, Link, NavLink } from "react-router-dom"
 import About from './pages/about'
 import Home from './pages/home'
-import React from 'react'
+import Vans from './pages/vans'
+
+import './server'
 
 function App() {
 
@@ -14,11 +17,13 @@ function App() {
         <Link to='/' >#VANLIFE</Link>
         <nav>
           <Link to='/about' >About</Link>
+          <Link to='/vans' >Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </>
 
