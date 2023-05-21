a6988ff757c294c3d611692cad923f979b4677da
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-05-21 08:40:38 +0100

    Navlink updated

--- react-router-6/src/components/header.jsx
+++ react-router-6/src/components/header.jsx
@@ -1,13 +1,28 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 export default function Header() {
+
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
         <header>
             <Link to='/' >#VANLIFE</Link>
             <nav>
-                <Link to='/host'>Host</Link>
-                <Link to='/about' >About</Link>
-                <Link to='/vans' >Vans</Link>
+                <NavLink to='/host'
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >Host</NavLink>
+
+                <NavLink to='/about'
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >About</NavLink>
+
+                <NavLink to='/vans'
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >Vans</NavLink>
             </nav>
         </header>
     )
--- react-router-6/src/components/layout.jsx
+++ react-router-6/src/components/layout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header";
+import Footer from "./footer";
 
 export default function Layout() {
     return (
-        <div>
+        <div className="site-wrapper" >
             <Header />
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </div>
     )
 }
