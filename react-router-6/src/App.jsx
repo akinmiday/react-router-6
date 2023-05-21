5499cdad9af41a6093cd90d33dfaccde623784d7
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-05-21 06:52:40 +0100

    Layout added

--- react-router-6/src/App.jsx
+++ react-router-6/src/App.jsx
@@ -5,28 +5,23 @@ import About from './pages/about'
 import Home from './pages/home'
 import Vans from './pages/vans'
 import VanDetail from './pages/vandetail'
-
 import './server'
+import Layout from './components/layout'
+import Header from './components/header'
 
 
 function App() {
 
-
   return (
 
     <>
-      <header>
-        <Link to='/' >#VANLIFE</Link>
-        <nav>
-          <Link to='/about' >About</Link>
-          <Link to='/vans' >Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />} >
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
     </>
 
